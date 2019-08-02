e138b4362875bbda7c60946a223101129673bd73
lusob <luis@sobrecueva.com>
2019-08-02 16:12:51 +0200

    Added new challenge solution

--- hackerrank.py
+++ hackerrank.py
@@ -964,3 +964,30 @@ if __name__ == '__main__':
     result = substrCount(n, s)
     fptr.write(str(result) + '\n')
     fptr.close()
+
+# https://www.hackerrank.com/challenges/2d-array/problem
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the hourglassSum function below.
+def hourglassSum(arr):
+    s = -999999999999
+    for i in range(4):
+        for j in range(4):
+            s = max(sum(arr[i][j:j+3])+arr[i+1][j+1]+sum(arr[i+2][j:j+3]), s)
+    return s
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+    arr = []
+    for _ in range(6):
+        arr.append(list(map(int, input().rstrip().split())))
+    result = hourglassSum(arr)
+    fptr.write(str(result) + '\n')
+    fptr.close()
